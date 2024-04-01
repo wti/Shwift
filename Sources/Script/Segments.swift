08bdb26bf692986b6e97e0139b72f82aff9a9674
wti <wti@users.noreply.github.com>
2024-04-01 15:55:59 -0700

    Segmented input: surface AsyncIterator API (for async-algorithms?)

--- Sources/Script/Segments.swift
+++ Sources/Script/Segments.swift
@@ -1,15 +1,27 @@
-public func reduceSegments<T>(
-  separatedBy delimiter: Character,
-  into initialResult: T,
-  _ updateAccumulatingResult: @escaping (inout T, String) async throws -> Void
+import Shwift
+
+/// Make PipableCommand running over ``AsyncSequence`` ``Builtin/Input/Segments``.
+public func withSegments<T>(
+  separatedBy delimiter: String = "\n",
+  _ run: @escaping (Builtin.Input.Segments) async throws -> T
 ) -> Shell.PipableCommand<T> {
   Shell.PipableCommand {
     try await Shell.invoke { _, invocation in
       try await invocation.builtin { channel in
-        try await channel.input.segments(separatedBy: delimiter)
-          .reduce(into: initialResult, updateAccumulatingResult)
+        try await run(channel.input.segments(separatedBy: delimiter))
       }
     }
+  }
+}
+
+/// Make PipableCommand reducing segmented input to some value (ending the pipe)
+public func reduceSegments<T>(
+  separatedBy delimiter: String,
+  into initialResult: T,
+  _ updateAccumulatingResult: @escaping (inout T, String) async throws -> Void
+) -> Shell.PipableCommand<T> {
+  withSegments(separatedBy: delimiter) {
+    try await $0.reduce(into: initialResult, updateAccumulatingResult)
   }
 }
 
